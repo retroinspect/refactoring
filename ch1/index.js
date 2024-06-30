a4e9997000239f7146f65e8419c926f9c793416b
retroinspect <sangachoi@kaist.ac.kr>
2024-07-01 00:13:28 +0900

    ch1

--- ch1/index.js
+++ ch1/index.js
@@ -25,17 +25,19 @@ function renderPlainText(data) {
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(aNumber / 100);
     }
-
-
 }
 
 function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData)
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -79,14 +81,13 @@ function statement(invoice, plays) {
     }
 
     function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 
     function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0)
+        return data.performances.reduce((total, p) => total + p.amount, 0);
     }
 
-    return renderPlainText(statementData, plays);
 }
 
 const invoices = require('./invoices.json');
