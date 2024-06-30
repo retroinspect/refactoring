46dd58a844d15b7badfcd9fc01183470b78eb5ff
retroinspect <sangachoi@kaist.ac.kr>
2024-06-30 16:53:53 +0900

    ch1: move function

--- ch1/index.js
+++ ch1/index.js
@@ -10,28 +10,6 @@ function print(text, color = "reset") {
     console.log(colors["reset"]);
 }
 
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 3000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`Unknown genre: ${play.type}`);
-    }
-    return result;
-}
 
 function statement(invoice, plays) {
     let totalAmount = 0;
@@ -41,6 +19,29 @@ function statement(invoice, plays) {
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance, play) {
+        let result = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 3000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`Unknown genre: ${play.type}`);
+        }
+        return result;
     }
 
     for (let perf of invoice.performances) {
