2572337c1cb1fe9f3696ac4f7548dbf184643704
retroinspect <sangachoi@kaist.ac.kr>
2024-06-30 16:54:56 +0900

    ch1: change function declaration (6.5)

--- ch1/index.js
+++ ch1/index.js
@@ -21,7 +21,7 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
 
         switch (playFor(aPerformance).type) {
